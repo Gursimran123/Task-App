f73ed14061ee5986a1960a36c03eda10003eb519
Gursimran Singh <54260630+Gursimran123@users.noreply.github.com>
2024-05-12 17:40:01 +0530

    Update Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
      const handleSubmit = async (e) => {
        e.preventDefault();
        try {
-         const response = await fetch("https://task-app-api-two.vercel.app/api/login", {
+         const response = await fetch("http://localhost:4000/api/login", {
            method: "POST",
            headers: {
              "Content-Type": "application/json",
